12f1786c30adeb46f9d3a9094469a3bb5ce0659a
Artur Lepioszka <arturlepioszka@gmail.com>
2023-10-06 22:22:06 +0200

    style: black, isort

--- tetris.py
+++ tetris.py
@@ -1,6 +1,7 @@
 import pygame
+from pygame.constants import *
+
 from game_engine import GameEngine
-from pygame import *
 
 
 pygame.init()
@@ -18,11 +19,16 @@ def tetris():
             if game_engine.run_event() is None:
                 return menu
         elif event.type == KEYDOWN:
-            if event.key == K_SPACE: game_engine.fall_down()
-            elif event.key == K_w or event.key == K_UP: game_engine.puzzle.update()
-            elif event.key == K_s or event.key == K_DOWN: game_engine.direction = pygame.Vector2(0,1)
-            elif event.key == K_a or event.key == K_LEFT: game_engine.direction = pygame.Vector2(-1,0)
-            elif event.key == K_d or event.key == K_RIGHT: game_engine.direction = pygame.Vector2(1,0)
+            if event.key == K_SPACE:
+                game_engine.fall_down()
+            elif event.key == K_w or event.key == K_UP:
+                game_engine.puzzle.update()
+            elif event.key == K_s or event.key == K_DOWN:
+                game_engine.direction = pygame.Vector2(0, 1)
+            elif event.key == K_a or event.key == K_LEFT:
+                game_engine.direction = pygame.Vector2(-1, 0)
+            elif event.key == K_d or event.key == K_RIGHT:
+                game_engine.direction = pygame.Vector2(1, 0)
 
 
 @game_engine.fps_loop_decorator
@@ -33,7 +39,7 @@ def menu():
         elif event.type == KEYDOWN:
             if event.key == K_RETURN:
                 return tetris
-    game_engine.screen.blit(game_engine.text_start, (55,110,200,200))
+    game_engine.screen.blit(game_engine.text_start, (55, 110, 200, 200))
 
 
 menu()
