3890678896f9fc6538062c7b8b75ce6809100aa4
Artur Lepioszka <arturlepioszka@gmail.com>
2023-10-24 20:31:17 +0200

    refactor: use classes not their instances

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,14 @@
 from .puzzle import I, J, L, O, S, T, Z
 
 
-puzzle_t = T()
-puzzle_o = O()
-puzzle_s = S()
-puzzle_z = Z()
-puzzle_i = I()
-puzzle_l = L()
-puzzle_j = J()
-puzzles = [puzzle_t, puzzle_i, puzzle_j, puzzle_l, puzzle_o, puzzle_z, puzzle_s]
-
-
+puzzles = [T, O, S, Z, I, L, J]
 __all__ = [
-    "puzzle_t",
-    "puzzle_o",
-    "puzzle_s",
-    "puzzle_z",
-    "puzzle_i",
-    "puzzle_l",
-    "puzzle_j",
+    "T",
+    "O",
+    "S",
+    "Z",
+    "I",
+    "L",
+    "J",
     "puzzles",
 ]
--- models/puzzle.py
+++ models/puzzle.py
@@ -4,102 +4,103 @@ from constants import BLUE, EMERLAND, GREEN, PINK, PURPLE, RED, YELLOW
 
 
 class BasePuzzle:
-    def regroup(self):
-        column, row = self.locus[0]
-        self.locus = [(column + x, row + y) for x, y in self.current_formation]
-        self.next_locus = [(column + x, row + y) for x, y in self.next_formation]
+    locus: list[tuple[int, int]]
+    next_locus: list[tuple[int, int]]
+    available_formations: list[list[tuple[int, int]]]
+    formations: itertools.cycle
+    current_formation: list[tuple[int, int]]
+    next_formation: list[tuple[int, int]]
+    color: tuple[int, int, int]
 
-    def change_formation(self, available_matrix: set):
-        if available_matrix >= set(self.next_locus):
-            self.current_formation = self.next_formation
-            self.next_formation = next(self.formations)
-            self.regroup()
+    def __init_subclass__(cls, **kwargs):
+        cls.reset()
 
-    def move(self, direction: tuple[int, int], available_matrix: set):
+    @classmethod
+    def regroup(cls):
+        column, row = cls.locus[0]
+        cls.locus = [(column + x, row + y) for x, y in cls.current_formation]
+        cls.next_locus = [(column + x, row + y) for x, y in cls.next_formation]
+
+    @classmethod
+    def change_formation(cls, available_matrix: set):
+        if available_matrix >= set(cls.next_locus):
+            cls.current_formation = cls.next_formation
+            cls.next_formation = next(cls.formations)
+            cls.regroup()
+
+    @classmethod
+    def move(cls, direction: tuple[int, int], available_matrix: set):
         column, row = direction
-        if available_matrix >= {(column + x, row + y) for x, y in self.locus}:
-            x, y = self.locus[0]
-            self.locus[0] = (column + x, row + y)
-            self.regroup()
+        if available_matrix >= {(column + x, row + y) for x, y in cls.locus}:
+            x, y = cls.locus[0]
+            cls.locus[0] = (column + x, row + y)
+            cls.regroup()
             return True
         return False
 
-    def reset(self):
-        self.locus = [(5, -2)]
-        self.formations = itertools.cycle(self.available_formations)
-        self.current_formation = next(self.formations)
-        self.next_formation = next(self.formations)
-        self.regroup()
+    @classmethod
+    def reset(cls):
+        cls.locus = [(5, -2)]
+        cls.formations = itertools.cycle(cls.available_formations)
+        cls.current_formation = next(cls.formations)
+        cls.next_formation = next(cls.formations)
+        cls.regroup()
 
 
 class T(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [
-            [(0, 0), (-1, 1), (0, 1), (1, 1)],
-            [(0, 0), (1, -1), (1, 0), (1, 1)],
-            [(0, 0), (-1, 0), (0, 1), (1, 0)],
-            [(0, 0), (-1, -1), (-1, 0), (-1, 1)],
-        ]
-        self.color = RED
-        self.reset()
+    available_formations = [
+        [(0, 0), (-1, 1), (0, 1), (1, 1)],
+        [(0, 0), (1, -1), (1, 0), (1, 1)],
+        [(0, 0), (-1, 0), (0, 1), (1, 0)],
+        [(0, 0), (-1, -1), (-1, 0), (-1, 1)],
+    ]
+    color = RED
 
 
 class O(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [[(0, 0), (0, -1), (1, 0), (1, -1)]]
-        self.color = BLUE
-        self.reset()
+    available_formations = [[(0, 0), (0, -1), (1, 0), (1, -1)]]
+    color = BLUE
 
 
 class S(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [
-            [(0, 0), (-1, 1), (0, 1), (1, 0)],
-            [(0, 0), (-1, -1), (-1, 0), (0, 1)],
-        ]
-        self.color = GREEN
-        self.reset()
+    available_formations = [
+        [(0, 0), (-1, 1), (0, 1), (1, 0)],
+        [(0, 0), (-1, -1), (-1, 0), (0, 1)],
+    ]
+    color = GREEN
 
 
 class Z(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [
-            [(0, 0), (-1, 0), (0, 1), (1, 1)],
-            [(0, 0), (0, -1), (-1, 0), (-1, 1)],
-        ]
-        self.color = PINK
-        self.reset()
+    available_formations = [
+        [(0, 0), (-1, 0), (0, 1), (1, 1)],
+        [(0, 0), (0, -1), (-1, 0), (-1, 1)],
+    ]
+    color = PINK
 
 
 class I(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [
-            [(0, 0), (-1, 0), (1, 0), (2, 0)],
-            [(0, 0), (0, -1), (0, -2), (0, -3)],
-        ]
-        self.color = YELLOW
-        self.reset()
+    available_formations = [
+        [(0, 0), (-1, 0), (1, 0), (2, 0)],
+        [(0, 0), (0, -1), (0, -2), (0, -3)],
+    ]
+    color = YELLOW
 
 
 class L(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [
-            [(0, 0), (0, -2), (0, -1), (1, 0)],
-            [(0, 0), (0, -1), (1, -1), (2, -1)],
-            [(0, 0), (-1, -2), (0, -1), (0, -2)],
-            [(0, 0), (1, -1), (1, 0), (-1, 0)],
-        ]
-        self.color = EMERLAND
-        self.reset()
+    available_formations = [
+        [(0, 0), (0, -2), (0, -1), (1, 0)],
+        [(0, 0), (0, -1), (1, -1), (2, -1)],
+        [(0, 0), (-1, -2), (0, -1), (0, -2)],
+        [(0, 0), (1, -1), (1, 0), (-1, 0)],
+    ]
+    color = EMERLAND
 
 
 class J(BasePuzzle):
-    def __init__(self):
-        self.available_formations = [
-            [(0, 0), (0, -2), (0, -1), (-1, 0)],
-            [(0, 0), (0, -1), (-1, -1), (-2, -1)],
-            [(0, 0), (1, -2), (0, -1), (0, -2)],
-            [(0, 0), (-1, -1), (1, 0), (-1, 0)],
-        ]
-        self.color = PURPLE
-        self.reset()
+    available_formations = [
+        [(0, 0), (0, -2), (0, -1), (-1, 0)],
+        [(0, 0), (0, -1), (-1, -1), (-2, -1)],
+        [(0, 0), (1, -2), (0, -1), (0, -2)],
+        [(0, 0), (-1, -1), (1, 0), (-1, 0)],
+    ]
+    color = PURPLE
